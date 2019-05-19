225e7db5ce696565940877f13b2e1a44e13f9bd0
Myoung-Wan Koo <mwkoo9@gmail.com>
2019-05-19 18:24:34 +0900

    Initial Commit

---
+++ MarsV1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarsV1
+//
+//  Created by Myoung-Wan Koo on 19/05/2019.
+//  Copyright © 2019 Myoung-Wan Koo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
